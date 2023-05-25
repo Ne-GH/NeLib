90e5e680c819faf4da867cc25f88318102bd556d
永恒之蓝 <61956889+Ne-GH@users.noreply.github.com>
2023-05-25 20:11:46 +0800

    摒弃STL，使用位图以最简方法实现

--- MemoryPool/MemoryPool.hpp
+++ MemoryPool/MemoryPool.hpp
@@ -10,99 +10,95 @@
 #include <memory>
 #include <map>
 
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+
+using namespace std;
 class MemoryPool {
-    size_t _memory_pool_size;
-    void *_memory_pool_address;
-    std::multimap<size_t,void *> _free_map;
-    std::multimap<void* ,size_t> _alloc_map;
+	size_t _memory_block_size = 4;
+	size_t _memory_pool_size = 10240;
+	void* _memory_pool_address = nullptr;
 
-
-    MemoryPool(const MemoryPool &) = default;
-    MemoryPool& operator = (const MemoryPool &) = default;
+	void* _bit_map_address = nullptr;
+	size_t _bit_map_size = 0;
+	MemoryPool(size_t memory_size = 10240) {
+		_memory_pool_size = memory_size;
+		_memory_pool_address = malloc(_memory_pool_size);
+		_bit_map_size = _memory_pool_size / _memory_block_size;	// 此处位图简单实现，使用byte代替bit
+		_bit_map_address = calloc(_bit_map_size,sizeof(char));
+	}
+	MemoryPool(const MemoryPool&) = default;
+	MemoryPool& operator = (const MemoryPool&) = default;
 public:
-    MemoryPool(size_t memory_pool_size = 10240) {
-        std::cout << "1" << std::endl;      // debug
-        _memory_pool_size = memory_pool_size;
-        _memory_pool_address = (void *)malloc(_memory_pool_size);
-        // _memory_pool_address = new char[_memory_pool_size];      // 改用malloc
-        _free_map.insert({_memory_pool_size,_memory_pool_address});
-    }
-    ~MemoryPool() {
-        free(_memory_pool_address);
-        _memory_pool_address = nullptr;
-    }
-    static MemoryPool& GetInstance() {
-        static std::unique_ptr<MemoryPool> _instance = std::make_unique<MemoryPool>();
-        return *_instance;
-    }
+	~MemoryPool() {
+		free(_memory_pool_address);
+		_memory_pool_address = nullptr;
+		free(_bit_map_address);
+		_bit_map_address = nullptr;
+	}
+	static MemoryPool& GetInstance() {
+		static MemoryPool *instance = nullptr;
+		if (instance == nullptr) {
+			instance = (MemoryPool *)malloc(sizeof(MemoryPool));
+			instance->MemoryPool::MemoryPool();//手动构造
+		}
+		return *instance;
+	}
 
-    void *Malloc(size_t size) {
-        auto it = _free_map.upper_bound(size);
-        // 没有空闲内存,调用malloc
-        if (it == _free_map.end()) {
-            return malloc(size);
-        }
-        // 找到适合的内存
-        _free_map.insert({it->first - size,(char *)(it->second) + size});
-        _alloc_map.insert({it->second,size});
-        auto ret_address = it->second;
-        it = _free_map.erase(it);
-        return ret_address;
-    }
+	void* Malloc(size_t size) {
+		if (size > 4) {
+			return malloc(size);
+		}
+		for (int i = 0; i < _bit_map_size; ++i) {
+			// 可以分配
+			if (((char*)_bit_map_address)[i] == 0) {
+				*((char*)_bit_map_address + i) = 1;
+				return (char *)_memory_pool_address + i;
+			}
+		}
+		// 无可分配内存；
+		return malloc(size);
 
-    void Delete(void *ptr) {
-        auto it = _alloc_map.find(ptr);
-        // 不在内存池中
-        if (it == _alloc_map.end()) {
-            free(ptr);
-        }
-        // 在内存池中
-        auto del_it = _free_map.insert({it->second,it->first});
-        _alloc_map.erase(it);
+	}
+	void Delete(void *ptr) {
+		if (ptr < _memory_pool_address 
+		|| ptr >= (char *)_memory_pool_address + _memory_pool_size) {
+			return free(ptr);
+		}
 
-        // 合并
-        std::vector<decltype(_free_map.end())> vec;
-        for (auto tmp = _free_map.begin();tmp != _free_map.end(); ++tmp) {
-            // 低地址相邻
-            if ((char *)tmp->second + tmp->first == del_it->second) {
-                vec.push_back(tmp);
-                break;
-            }
-            // 高地址相邻
-            if ((char *)del_it->second + del_it->first == tmp->second) {
-                vec.push_back(tmp);
-                break;
-            }
-        }
-        vec.push_back(del_it);
-        std::sort(vec.begin(),vec.end(),[&](decltype(_free_map.end()) a, decltype(_free_map.end()) b) -> bool {
-            if (a->second < b->second)
-                return true;
-            return false;
-        });
-        auto start_address = vec[0]->second;
-        size_t new_size = 0;
-        for (auto tmp : vec) {
-            new_size += tmp->first;
-            _free_map.erase(tmp);
-        }
-        _free_map.insert({new_size,start_address});
-
-    }
+		int index = (intptr_t)ptr - (intptr_t)_memory_pool_address;
+		((char*)_bit_map_address)[index] = 0;
+		return;
+	}
 
 };
 
-void *operator new(size_t size) {
-    return MemoryPool::GetInstance().Malloc(size);
-}
-void *operator new[](size_t size) {
-    return MemoryPool::GetInstance().Malloc(size);
-}
-void operator delete(void *ptr) noexcept {
-    return MemoryPool::GetInstance().Delete(ptr);
-}
-void operator delete[](void *ptr) noexcept {
-    return MemoryPool::GetInstance().Delete(ptr);
+
+//void* operator new(size_t size) {
+//	return MemoryPool::GetInstance().Malloc(size);
+//}
+//void* operator new[](size_t size) {
+//	return MemoryPool::GetInstance().Malloc(size);
+//}
+//void operator delete(void* ptr) {
+//	return MemoryPool::GetInstance().Delete(ptr);
+//}
+//void operator delete[](void* ptr) {
+//	return MemoryPool::GetInstance().Delete(ptr);
+//}
+
+void Test() {
+	vector <int*> vec(2550);
+	for (auto& p : vec) {
+		p = new int;
+	}
+
+	for (auto it = vec.rbegin(); it != vec.rend(); ++it) {
+		delete *it;
+	}
+	return;
 }
 
 #endif //MEMORYPOOL_MEMORYPOOL_HPP
